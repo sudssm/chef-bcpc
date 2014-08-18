6809b07cc04234fed4c3a2aa2df87953884b4b0e
Sudarshan Muralidhar <sudarshan.muralidhar@gmail.com>
2014-08-18 19:02:43 -0400

    one-line build-bins conditional

--- cookbooks/bcpc/files/default/build_bins.sh
+++ cookbooks/bcpc/files/default/build_bins.sh
@@ -195,16 +195,13 @@ fi
 cert="cacert.pem"
 if [[ -e $cert ]]; then
     echo "Using $cert as pip cert file"
+    cert="--cert $cert"
 else
-    cert=""
     echo "Not using a pip cert file. If behind a MITM proxy, put a $cert in `pwd`"
+    cert=""
 fi
 for package in requests-aws==0.1.5 "httplib2>=0.7.5" http://tarballs.openstack.org/sahara/sahara-stable-icehouse.tar.gz sahara-dashboard python-saharaclient; do
-    if [[ -z "$cert" ]]; then
-        pip install --no-use-wheel --upgrade -d $PIPDIR $package
-    else
-        pip --cert $cert install --no-use-wheel --upgrade -d $PIPDIR $package
-    fi
+    pip $cert install --no-use-wheel --upgrade -d $PIPDIR $package
 done
 FILES="$PIPDIR $FILES"
 
